4fbfa231ca7e22b9d91da6545d89922f155d76bb
Madhan Kumaar <madhankumaar96@gmail.com>
2025-04-18 19:44:46 +0530

    Update UserRepo.java

--- demo/src/main/java/com/LoginAuth/demo/repository/UserRepo.java
+++ demo/src/main/java/com/LoginAuth/demo/repository/UserRepo.java
@@ -6,3 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }
+//JPA repo has inbuilt functions that we can use which is equivalent to SQL queries
